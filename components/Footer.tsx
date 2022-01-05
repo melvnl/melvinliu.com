a9f98372922ca232e32aa8e38d90cef8a70f6dce
himelv <63998835+melvnl@users.noreply.github.com>
2022-01-05 23:18:09 +0700

    fix: footer link color unchanged in projects page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,12 @@
-const ExternalLink = ({ href, children }: any) => (
+import { useRouter } from "next/router";
+import cn from "classnames";
+
+const ExternalLink = ({ href, children, isProjects }: any) => (
   <a
-    className="text-gray-600 hover:text-primaryRed transition"
+    className={cn(
+      isProjects ? "text-white" : "text-gray-600",
+      "hover:text-primaryRed transition"
+    )}
     target="_blank"
     rel="noopener noreferrer"
     href={href}
@@ -10,28 +16,42 @@ const ExternalLink = ({ href, children }: any) => (
 );
 
 export default function Footer() {
+  const router = useRouter();
+  const isProjects = router.pathname === "/projects";
+
   return (
     <footer className="flex flex-col justify-center items-start mx-auto w-full mt-20 mb-8 ">
       <hr className="w-full border-1 border-gray-600 mb-8" />
       <div className="w-full max-w-2xl">
         <div className="flex flex-col md:flex-row">
           <div className="mb-4 md:mb-0 mr-8">
-            <p className="text-gray-600 hover:text-primaryRed transition">
-              @2021
+            <p className={cn(isProjects ? "text-white" : "text-gray-600")}>
+              ©️ 2021
             </p>
           </div>
           <div className="mb-4 md:mb-0 mr-8">
-            <ExternalLink href="https://twitter.com/mlven23">
+            <ExternalLink
+              href="https://twitter.com/mlven23"
+              isProjects={isProjects}
+            >
               Twitter
             </ExternalLink>
           </div>
           <div className="mb-4 md:mb-0 mr-8">
-            <ExternalLink href="https://www.linkedin.com/in/melvin-liu/">
+            <ExternalLink
+              href="https://www.linkedin.com/in/melvin-liu/"
+              isProjects={isProjects}
+            >
               LinkedIn
             </ExternalLink>
           </div>
           <div className="mb-4 md:mb-0 mr-8">
-            <ExternalLink href="https://github.com/melvnl">Github</ExternalLink>
+            <ExternalLink
+              href="https://github.com/melvnl"
+              isProjects={isProjects}
+            >
+              Github
+            </ExternalLink>
           </div>
         </div>
       </div>
