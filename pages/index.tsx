5702d0a9d4bbda33582c8b132d2699f24130a6b1
himelv <63998835+melvnl@users.noreply.github.com>
2022-01-09 22:12:17 +0700

    revise homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import Image from "next/image";
+// import Image from "next/image";
 import Container from "../components/Container";
 
 const Home: NextPage = () => {
@@ -7,75 +7,92 @@ const Home: NextPage = () => {
     <Container>
       <div className=" bg-primaryWhite">
         <div className=" text-primaryBlack ">
-          <h1 className=" font-serif font-semibold text-2xl md:text-5xl mb-9">
-            Melvin Liu, Software Engineer by Nature.
+          <h1 className=" font-sans font-bold text-3xl md:text-5xl mb-10 md:mb-20">
+            Move Fast, Break Things
           </h1>
-          <div className="max-w-[672px]">
-            <p className=" font-semibold text-base md:text-xl mb-4">
-              Halo, I’m currently a Software Engineer in an unicorn tech company
-              in Indonesia called{" "}
-              <a
-                className=" underline font-bold hover:text-primaryRed"
-                href="https://about.bukalapak.com/en/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                BUKALAPAK
-              </a>
-              , the first startup that listed in Indonesia’s stock market, to
-              bring a fair economy for all through online commerce.{" "}
-            </p>
-            <p className=" font-semibold text-base md:text-xl mb-4">
-              Previously, I joined{" "}
-              <a
-                className=" underline font-bold hover:text-primaryRed"
-                href="https://tanifund.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Tanifund
-              </a>{" "}
-              as an Engineer, with the focus on connecting farmers with market
-              while ensuring their access to financial (through P2P lending
-              system) and technical supports along with Tanihub.{" "}
-            </p>
-            <p className=" font-semibold text-base md:text-xl mb-4">
-              Along with that, since 2020, I’ve enabled more than 20+
-              Micro,Small, and Medium Entreprises gain digital advantage through
-              reliable, and well designed website / app.
-            </p>
-          </div>
-        </div>
-        <div className=" flex mt-12 overflow-x-scroll">
-          <div className=" flex-shrink-0">
-            <Image
-              src="/static/images/home/img-1.png"
-              className=" flex-shrink-0"
-              alt="Melvin Liu, Image 1"
-              width={480}
-              height={480}
-              quality={100}
-            />
-          </div>
-          <div className=" flex-shrink-0 ml-5">
-            <Image
-              src="/static/images/home/img-2.png"
-              className=" flex-shrink-0 ml-5"
-              alt="Melvin Liu, Image 2"
-              width={853}
-              height={480}
-              quality={100}
-            />
-          </div>
-          <div className=" flex-shrink-0 ml-5">
-            <Image
-              src="/static/images/home/img-3.png"
-              className=" flex-shrink-0 ml-5"
-              alt="Melvin Liu, Image 3"
-              width={480}
-              height={480}
-              quality={100}
-            />
+          <div className=" text-primaryBlack font-normal grid grid-cols-1 md:grid-cols-3 gap-5">
+            <div className=" text-left">
+              <p className=" mb-3">
+                Hi, I&#39;m Melvin Liu, a Software Engineer who currently reside
+                in Indonesia. I&#39;ve been designing and building web
+                application since 2019, it all started from a simple landing
+                page for a local raw food shipment company, to a large scale
+                e-commerce that serve million of users.
+              </p>
+              <p className=" mb-3">
+                In the past, I was lucky enough to start my software engineering
+                career in few notable company in Indonesia, the most notable one
+                is{" "}
+                <a
+                  className=" underline text-primaryRed"
+                  href="https://tanifund.com/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  Bukalapak
+                </a>{" "}
+                - Indonesia first unicorn startup goes IPO (Initial Public
+                Offering), once started out as marketplace platform and grew
+                into an All-Commerce platform, empowering both users and
+                communities as one company: Buka.
+              </p>
+
+              <p></p>
+            </div>
+            <div className=" border-l-0 md:border-l pl-0 md:pl-4 border-gray-600 text-left">
+              <p className=" mb-3">
+                My main expertise is web development, especially in front-end
+                engineering with Vue.js/ Nuxt.js, Vuex, Tailwind, SASS/SCSS. I
+                don&#39;t limit myself to certain technology, hence I can do
+                React.js/Next.js and backend engineering as well either Node.js
+                or Go
+              </p>
+              <p className="mb-3">
+                Feel free to look at my side project that I make public on{" "}
+                <a
+                  className=" underline text-primaryRed"
+                  href="https://github.com/melvnl"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  Github
+                </a>
+              </p>
+            </div>
+            <div className=" border-l-0 md:border-l pl-0 md:pl-4  border-gray-600 text-left">
+              <p className=" mb-3">
+                If you have read this far, let&#39;s be friends! Connect with me
+                on{" "}
+                <span>
+                  <a
+                    className=" underline text-primaryRed"
+                    href="https://github.com/melvnl"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    Instagram
+                  </a>
+                  ,{" "}
+                  <a
+                    className=" underline text-primaryRed"
+                    href="https://www.linkedin.com/in/melvin-liu/"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    Linkedin
+                  </a>
+                  , or{" "}
+                  <a
+                    className=" underline text-primaryRed"
+                    href="https://twitter.com/mlven23"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    Twitter
+                  </a>
+                </span>
+              </p>
+            </div>
           </div>
         </div>
       </div>
