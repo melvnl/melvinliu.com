ded995c4c5cc94de4558dab4ed89dcb14c3776c8
himelv <63998835+melvnl@users.noreply.github.com>
2022-01-13 23:35:50 +0700

    fix: change footer layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,20 @@
 import { useRouter } from "next/router";
 import cn from "classnames";
 
-const ExternalLink = ({ href, children, isProjects }: any) => (
-  <a
-    className={cn(
-      isProjects ? "text-white" : "text-gray-600",
-      "hover:text-primaryRed transition"
-    )}
-    target="_blank"
-    rel="noopener noreferrer"
-    href={href}
-  >
-    {children}
-  </a>
+const ExternalLink = ({ prefix, href, children, isProjects }: any) => (
+  <>
+    <span className={isProjects ? "text-white" : "text-primaryBlack"}>
+      {prefix}
+    </span>
+    <a
+      className="text-primaryRed underline"
+      target="_blank"
+      rel="noopener noreferrer"
+      href={href}
+    >
+      {children}
+    </a>
+  </>
 );
 
 export default function Footer() {
@@ -22,35 +24,66 @@ export default function Footer() {
   return (
     <footer className="flex flex-col justify-center items-start mx-auto w-full mt-20 mb-8 ">
       <hr className="w-full border-1 border-gray-600 mb-8" />
-      <div className="w-full max-w-2xl">
-        <div className="flex flex-col md:flex-row">
-          <div className="mb-4 md:mb-0 mr-8">
-            <p className={cn(isProjects ? "text-white" : "text-gray-600")}>
-              ©️ 2021
-            </p>
-          </div>
-          <div className="mb-4 md:mb-0 mr-8">
+      <div className="w-full flex flex-col md:flex-row ">
+        <div
+          className={cn(
+            isProjects ? "text-white" : "text-primaryBlack",
+            "flex flex-col mb-4 md:mb-0 md:w-1/2"
+          )}
+        >
+          <h1 className=" mb-2 font-semibold">Contact</h1>
+          <div className="">phone: +62-819-0881-6826</div>
+          <div className="">email: melvinliu2000@gmail.com</div>
+        </div>
+        <div
+          className={cn(
+            isProjects ? "text-white" : "text-primaryBlack",
+            "flex flex-col mb-4 md:mb-0 md:w-1/4"
+          )}
+        >
+          <h1 className=" mb-2 font-semibold">Social</h1>
+          <div className="">
             <ExternalLink
+              prefix="Twitter: "
               href="https://twitter.com/mlven23"
               isProjects={isProjects}
             >
-              Twitter
+              mlven23
             </ExternalLink>
           </div>
-          <div className="mb-4 md:mb-0 mr-8">
+          <div className="">
             <ExternalLink
+              prefix="Instagram: "
+              href="https://www.instagram.com/mlven23/"
+              isProjects={isProjects}
+            >
+              mlven23
+            </ExternalLink>
+          </div>
+          <div className="">
+            <ExternalLink
+              prefix="Linkedin: "
               href="https://www.linkedin.com/in/melvin-liu/"
               isProjects={isProjects}
             >
-              LinkedIn
+              Melvin
             </ExternalLink>
           </div>
-          <div className="mb-4 md:mb-0 mr-8">
+        </div>
+        <div
+          className={cn(
+            isProjects ? "text-white" : "text-primaryBlack",
+            "flex flex-col mb-4 md:mb-0 md:w-1/4"
+          )}
+        >
+          <h1 className=" mb-2 font-semibold">Work</h1>
+          <div className="mb-4 md:mb-0 ">
             <ExternalLink
+              prefix="Github: "
               href="https://github.com/melvnl"
               isProjects={isProjects}
             >
-              Github
+              melvnl
             </ExternalLink>
           </div>
         </div>
