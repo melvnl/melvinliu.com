bdca62f641b89d6696452760e96cb1570a45701b
himelv <63998835+melvnl@users.noreply.github.com>
2022-01-15 12:11:09 +0700

    add: filter blog by search input (#13)

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -5,10 +5,16 @@ import Container from "../components/Container";
 import BlogContainer from "../components/BlogContainer";
 import BlogCard from "../components/BlogCard";
 import { InferGetStaticPropsType } from "next";
+import { useState } from "react";
 
 export default function Blog({
   posts,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const [searchValue, setSearchValue] = useState("");
+  const filteredBlogPosts = posts.filter((post) =>
+    post.title.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
   return (
     <Container
       title="Blog – Melvin Liu"
@@ -25,8 +31,36 @@ export default function Blog({
         <h3 className="mt-8 mb-4 text-2xl font-bold tracking-tight text-black md:text-4xl ">
           All Posts
         </h3>
+        <div className="relative w-full mb-4">
+          <input
+            aria-label="Search articles"
+            type="text"
+            onChange={(e) => setSearchValue(e.target.value)}
+            placeholder="Search articles"
+            className="block w-full px-4 py-2 text-gray-900 bg-white border border-primaryGray rounded-md dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:text-gray-100"
+          />
+          <svg
+            className="absolute w-5 h-5 text-gray-400 right-3 top-3 dark:text-gray-300"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {posts.map((post) => (
+          {!filteredBlogPosts.length && (
+            <p className="mb-4 text-gray-600 dark:text-gray-400">
+              No posts found.
+            </p>
+          )}
+          {filteredBlogPosts.map((post) => (
             <BlogCard
               key={post.body.title}
               cover={post.body.cover}
@@ -47,13 +81,15 @@ export async function getStaticProps() {
   const files = fs.readdirSync(path.join("data/blog"));
 
   const posts = files.map((filename) => {
-    const slug = filename.replace(".md", "");
+    const title = filename.replace(".md", "").replace(/-/g, " ");
 
     const file = fs.readFileSync(path.join("data/blog", filename), "utf-8");
 
     const { data: body } = matter(file);
 
-    return { slug, body };
+    console.log(title);
+
+    return { title, body };
   });
 
   return { props: { posts: posts } };
