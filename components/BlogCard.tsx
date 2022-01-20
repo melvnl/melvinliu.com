35aa5c82510853c0d1b3f07d3e6447166668c538
himelv <63998835+melvnl@users.noreply.github.com>
2022-01-20 15:38:48 +0700

    chores: change blog card cover img witdh & quality

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -15,7 +15,13 @@ export default function BlogCard({
     <Link href={`/blog/${slug}`}>
       <a className="group   overflow-hidden border border-borderGray rounded-md">
         <div className="flex flex-col w-full">
-          <Image src={cover} width={350} height={140} alt={title} />
+          <Image
+            src={cover}
+            width={380}
+            height={140}
+            alt={title}
+            quality={100}
+          />
           <div className=" border-t border-borderGray p-4">
             <h4 className="w-full mb-2 text-lg font-bold text-primaryBlack md:text-2xl group-hover:text-primaryRed ">
               {title}
