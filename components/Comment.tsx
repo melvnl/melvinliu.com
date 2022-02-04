a6a6df676412a549f44a97ae7b17de2429507d1f
himelv <63998835+melvnl@users.noreply.github.com>
2022-02-04 15:57:18 +0700

    feat: add giscus comment component to blog slug

---
+++ components/Comment.tsx
@@ -0,0 +1,16 @@
+import { Giscus } from "@giscus/react";
+
+export default function Comment() {
+  return (
+    <Giscus
+      repo="melvnl/melvinliu.com"
+      repoId="R_kgDOGjYtbQ"
+      category="General"
+      categoryId="DIC_kwDOGjYtbc4CA_TS"
+      mapping="pathname"
+      reactionsEnabled="0"
+      emitMetadata="0"
+      theme="dark"
+    />
+  );
+}
