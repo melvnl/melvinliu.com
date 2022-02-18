fb1f389a906b3bbfc2fa61da6bc2093a5b9fa460
himelv <63998835+melvnl@users.noreply.github.com>
2022-02-18 12:13:29 +0700

    Integrate cypress (#30)
    * initialize cypress
    
    * feat: add local-cypress, test all page snapshot
    
    * chores: solve merge conflict
    
    * feat: add start-server-and-test dependencies
    
    * feat: cy test spotify feature
    
    * feat: cy test newsletter feature

---
+++ cypress/integration/pages/render.spec.ts
@@ -0,0 +1,39 @@
+import { describe, it, cy } from "local-cypress";
+
+describe("Check all page snapshot", () => {
+  it("Homepage", () => {
+    cy.visit("/");
+    cy.get("h1").should("contain", "Move Fast, Break Things");
+  });
+
+  it("Blog", () => {
+    cy.visit("/blog");
+    cy.get("h1").should("contain", "Blog");
+    cy.get("p").should(
+      "contain",
+      "Thoughts, career hacks, software-engineering, and front-end development."
+    );
+  });
+
+  it("Snippet", () => {
+    cy.visit("/snippet");
+    cy.get("h1").should("contain", "Code Snippets");
+    cy.get("span").should(
+      "contain",
+      "This is my own personal code library, where I store code snippet regarding various tech stack and tools, e.g: React, Next, Strapi, API , etc. You can use it freely!"
+    );
+  });
+
+  it("Inquiry", () => {
+    cy.visit("/inquiry");
+    cy.get("h1").should("contain", "Available for freelance projects");
+  });
+
+  it("Freelance", () => {
+    cy.visit("/freelance");
+    cy.get("h1").should(
+      "contain",
+      "I solve incredibly challenging software and design problems, here are just a few examples."
+    );
+  });
+});
