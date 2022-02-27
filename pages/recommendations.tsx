8ae89c9d026b029ce96587bf76e97b09fee7ea63
himelv <63998835+melvnl@users.noreply.github.com>
2022-02-27 20:56:03 +0700

    chores: delete login option

--- pages/recommendations.tsx
+++ pages/recommendations.tsx
@@ -112,14 +112,14 @@ const Recommendations: NextPage = () => {
           Give Me Your Recommendation!
         </h5>
         <p className="my-1 text-primaryBlack dark:text-white">
-          Share a message for my future client.
+          Share a message for my future client & colleague.
         </p>
         {!session && (
           <button
             className="flex items-center justify-center my-4 font-bold h-8 bg-gray-200 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded w-28"
             onClick={(e) => {
               e.preventDefault();
-              signIn();
+              signIn("github");
             }}
           >
             Login
