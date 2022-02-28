0de9f2c2e57796186f7b67ce54232f039be7b75e
himelv <63998835+melvnl@users.noreply.github.com>
2022-02-28 23:17:59 +0700

    initalize

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,13 +7,11 @@ import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="bg-primaryWhite dark:bg-darkBlack">
-      <SessionProvider session={pageProps.session}>
-        <ThemeProvider attribute="class">
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </SessionProvider>
-    </div>
+    <SessionProvider session={pageProps.session}>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
 
