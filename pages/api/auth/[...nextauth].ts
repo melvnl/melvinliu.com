acecb580b1257a7755eac6003826c6415e0109ec
himelv <63998835+melvnl@users.noreply.github.com>
2022-03-01 17:03:10 +0700

    Add google-auth provider (#37)
    * feat: add google provider in config nextauth.js
    
    * chores: update .env.sample

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,16 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.OAUTH_CLIENT_KEY,
       clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_KEY!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
   secret: process.env.OAUTH_SECRET,
--- pages/support.tsx
+++ pages/support.tsx
@@ -128,15 +128,54 @@ const Support: NextPage = () => {
           Share a message for my future client & colleague.
         </p>
         {!session && (
-          <button
-            className=" flex items-center my-4 font-semibold px-4 py-2 bg-gray-900 dark:bg-gray-700 text-white dark:text-white rounded "
-            onClick={(e) => {
-              e.preventDefault();
-              signIn("github");
-            }}
-          >
-            <FiGithub size={16} className=" mr-2" /> Login with Github
-          </button>
+          <div className=" mt-8 md:mt-0 flex flex-col md:flex-row">
+            <button
+              className=" flex items-center justify-center my-2 md:my-4 font-semibold px-4 py-2 bg-gray-900 dark:bg-gray-700 text-white dark:text-white rounded "
+              onClick={(e) => {
+                e.preventDefault();
+                signIn("github");
+              }}
+            >
+              <FiGithub size={16} className=" mr-2" /> Login with Github
+            </button>
+            <button
+              className=" flex items-center justify-center   my-2 md:my-4 md:ml-2 font-semibold px-4 py-2 bg-gray-100 dark:bg-gray-700 text-primaryBlack dark:text-white rounded "
+              onClick={(e) => {
+                e.preventDefault();
+                signIn("google");
+              }}
+            >
+              <div className=" h-4 w-4 mr-2">
+                <svg
+                  viewBox="0 0 533.5 544.3"
+                  focusable="false"
+                  role="presentation"
+                  aria-hidden="true"
+                  className="css-yxiis9"
+                >
+                  <g>
+                    <path
+                      d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z"
+                      fill="#4285f4"
+                    ></path>
+                    <path
+                      d="M272.1 544.3c73.4 0 135.3-24.1 180.4-65.7l-87.7-68c-24.4 16.6-55.9 26-92.6 26-71 0-131.2-47.9-152.8-112.3H28.9v70.1c46.2 91.9 140.3 149.9 243.2 149.9z"
+                      fill="#34a853"
+                    ></path>
+                    <path
+                      d="M119.3 324.3c-11.4-33.8-11.4-70.4 0-104.2V150H28.9c-38.6 76.9-38.6 167.5 0 244.4l90.4-70.1z"
+                      fill="#fbbc04"
+                    ></path>
+                    <path
+                      d="M272.1 107.7c38.8-.6 76.3 14 104.4 40.8l77.7-77.7C405 24.6 339.7-.8 272.1 0 169.2 0 75.1 58 28.9 150l90.4 70.1c21.5-64.5 81.8-112.4 152.8-112.4z"
+                      fill="#ea4335"
+                    ></path>
+                  </g>
+                </svg>
+              </div>{" "}
+              Login with Google
+            </button>
+          </div>
         )}
         {session?.user && (
           <>
