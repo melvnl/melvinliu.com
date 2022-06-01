23e31a870c21ff5c4c105140287ad74c96ba180c
himelv <63998835+melvnl@users.noreply.github.com>
2022-06-01 12:44:51 +0700

    update: add safari meta rel favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,25 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <link href="/static/images/favicon.svg" rel="shortcut icon" />
+        <link rel="apple-touch-icon" href="/static/images/favicon.svg" />
+        <link
+          href="/static/images/favicon-32x32.png"
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          href="/static/images/favicon-16x16.png"
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link
+          color="#E16259"
+          href="/static/images/safari-pinned-tab.svg"
+          rel="mask-icon"
+        />
       </Head>
       <body className="bg-primaryWhite dark:bg-darkBlack">
         <Main />
