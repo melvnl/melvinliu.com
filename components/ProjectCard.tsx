47cc2d94455a07b726235c754fa31c07b33e2789
himelv <63998835+melvnl@users.noreply.github.com>
2022-07-17 02:55:13 +0700

    init

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -12,27 +12,27 @@ export default function ProjectCard({
   banner,
 }: Project) {
   return (
-    // <Link href={`/work/${slug}`}>
-    //   <a className="">
-    <div className="wrapper mb-5 ">
-      <div className="img-wrapper mb-2 md:mb-8">
-        <Image
-          quality={100}
-          src={cover}
-          width="608"
-          height="400"
-          alt="Hero 1"
-        />
-        <h1 className="text-base font-medium text-primaryGray dark:text-gray-300">
-          {type}
-        </h1>
-      </div>
-      <div className="description text-primaryBlack text-left dark:text-white text-base max-w-[577px]">
-        <h2 className=" font-bold mb-2">{title}</h2>
-        <p className=" font-medium">{description}</p>
-      </div>
-    </div>
-    //   </a>
-    // </Link>
+    <Link href={`/work/${slug}`}>
+      <a className="">
+        <div className="wrapper mb-5 ">
+          <div className="img-wrapper mb-2 md:mb-8">
+            <Image
+              quality={100}
+              src={cover}
+              width="608"
+              height="400"
+              alt="Hero 1"
+            />
+            <h1 className="text-base font-medium text-primaryGray dark:text-gray-300">
+              {type}
+            </h1>
+          </div>
+          <div className="description text-primaryBlack text-left dark:text-white text-base max-w-[577px]">
+            <h2 className=" font-bold mb-2">{title}</h2>
+            <p className=" font-medium">{description}</p>
+          </div>
+        </div>
+      </a>
+    </Link>
   );
 }
