3702f3c42de6a2161c51688a8503f5a652f2ba66
himelv <63998835+melvnl@users.noreply.github.com>
2022-07-17 15:48:20 +0700

    feat: change error page image, and copywriting

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,24 +4,32 @@ import Container from "@/components/Container";
 
 export default function NotFound() {
   return (
-    <Container title="404 – Melvin Liu">
+    <Container title="Page Not Found">
       <div className="text-center mx-auto mb-16">
-        <Image
-          src="/static/images/404.png"
-          width={300}
-          height={300}
-          alt="Page not found"
-        />
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight my-4 text-primaryBlack dark:text-white ">
-          Whoa, you’re traveling too far!
+          404 Not Found
         </h1>
-        <p className="text-primaryGray dark:text-gray-300 mb-8">
-          This means you type the wrong url. Check the link again, or you can go
-          back to the homepage.
-        </p>
+        <p className="text-primaryGray dark:text-gray-300 mb-1">
+          Sepertinya anda tersesat. Nikmati dulu komik dari
+        </p>{" "}
+        <a
+          href="https://www.instagram.com/komikkamvret/"
+          className=" block underline font-bold mx-auto text-center rounded-md text-primaryBlack dark:text-white "
+        >
+          komikkamvret
+        </a>
+        <div className=" mx-auto mb-8 mt-4">
+          <Image
+            src="/static/images/komikkamvret.jpg"
+            width={540}
+            height={540}
+            alt="123"
+            quality={100}
+          />
+        </div>
         <Link href="/">
-          <a className="p-2 sm:p-4 w-64 font-bold mx-auto bg-gray-200 dark:bg-gray-800 text-center rounded-md text-primaryBlack dark:text-white ">
-            Return Home
+          <a className="p-2 sm:p-4 w-64 font-bold mx-auto bg-primaryRed text-center rounded-md text-white dark:text-primaryBlack">
+            Kembali ke Halaman Utama
           </a>
         </Link>
       </div>
