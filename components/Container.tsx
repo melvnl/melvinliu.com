364b7e206d8ec636692ce16522f5fe1488902e7e
himelv <63998835+melvnl@users.noreply.github.com>
2022-07-29 13:06:53 +0700

    feature/navbar (#55)
    * init
    
    * update navbar and mobile navbar

--- components/Container.tsx
+++ components/Container.tsx
@@ -10,10 +10,12 @@ import { HelloFlag } from "@/constants/env";
 
 import Footer from "./Footer";
 import { useEffect, useState } from "react";
+import MobileMenu from "./MobileNavbar";
 
 const links = [
   { href: "/", text: "Home" },
   { href: "/work", text: "Work" },
+  { href: "/blog", text: "Blog" },
   { href: "/dashboard", text: "Dashboard" },
   { href: "/support", text: "Support" },
 ];
@@ -31,7 +33,7 @@ function NavItem({ href, text }: Nav) {
           isActive
             ? "font-semibold text-gray-800 dark:text-white "
             : "font-normal text-gray-600 dark:text-gray-300",
-          " p-1 md:ml-4 sm:px-3 sm:py-2 rounded-sm hover:bg-bgHovered dark:hover:bg-gray-700 transition-all"
+          " hidden md:inline-block p-1 md:mr-7 sm:px-3 sm:py-2 sm:pl-0 rounded-sm hover:bg-bgHovered dark:hover:bg-gray-700 transition-all"
         )}
       >
         <span className="capsize">{text}</span>
@@ -100,55 +102,55 @@ export default function Container(props: any) {
       <div
         className={cn(
           onTop ? "" : "shadow-sm dark:shadow-gray-300 ",
-          "bg-primaryWhite dark:bg-darkBlack sticky top-0 z-50 border-b  border-primaryRed md:border-0 px-2 xl:px-24 mb-4"
+          "bg-primaryWhite dark:bg-darkBlack sticky top-0 z-50 md:border-b   md:border-0 px-2 xl:px-24 mb-4"
         )}
       >
-        <nav className=" flex justify-between items-center w-full relative mx-auto py-4 ">
-          <div className=" hidden font-bold md:block text-2xl whitespace-nowrap text-primaryBlack dark:text-white">
-            melvinliu.com
-          </div>
+        <nav className=" flex justify-between items-center w-full relative  py-4 ">
           <div className=" w-full flex justify-between items-center md:w-auto ">
-            {links.map(({ href, text }: Nav) => (
-              <NavItem key={text} href={href} text={text} />
-            ))}
-            <button
-              aria-label="Toggle Dark Mode"
-              type="button"
-              className="w-9 h-9 md:ml-4 bg-gray-200 rounded-lg dark:bg-gray-600 flex items-center justify-center  hover:ring-2 ring-gray-300  transition-all"
-              onClick={() =>
-                setTheme(resolvedTheme === "light" ? "dark" : "light")
-              }
-            >
-              {mounted && (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  className="w-5 h-5 text-gray-800 dark:text-gray-200"
-                >
-                  {resolvedTheme === "dark" ? (
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                    />
-                  ) : (
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                    />
-                  )}
-                </svg>
-              )}
-            </button>
+            <div className=" w-full">
+              <MobileMenu />
+              {links.map(({ href, text }: Nav) => (
+                <NavItem key={text} href={href} text={text} />
+              ))}
+            </div>
           </div>
+          <button
+            aria-label="Toggle Dark Mode"
+            type="button"
+            className="w-9 h-9 md:ml-4 bg-gray-200 rounded-lg dark:bg-gray-600 flex items-center justify-center  hover:ring-2 ring-gray-300  transition-all"
+            onClick={() =>
+              setTheme(resolvedTheme === "light" ? "dark" : "light")
+            }
+          >
+            {mounted && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                className="w-5 h-5 text-gray-800 dark:text-gray-200"
+              >
+                {resolvedTheme === "dark" ? (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                  />
+                ) : (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                  />
+                )}
+              </svg>
+            )}
+          </button>
         </nav>
       </div>
-      <main className=" flex flex-col justify-center px-2 xl:px-24 pt-4 mt-8 ">
+      <main className=" flex flex-col justify-center px-2 xl:px-24 mt-2 md:mt-8 pt-2 md:pt-4  ">
         {children}
         <Footer />
       </main>
