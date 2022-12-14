744a123c6f37b4f12bf998bfbdae5907f0c72068
melvnl <melvinlim2000@gmail.com>
2022-12-14 09:19:19 +0700

    chores: properly adjust dynamic meta property for blog slug page

--- components/Container.tsx
+++ components/Container.tsx
@@ -80,32 +80,39 @@ export default function Container(props: any) {
     saidHello = true;
   }
 
+  const isBlogSlug = router.asPath.startsWith("/blog/");
+
   return (
     <div className="w-full">
-      <Head>
-        <title>{meta.title}</title>
-        <meta name="robots" content="follow, index" />
-        <meta content={meta.description} name="description" />
-        <meta
-          property="og:url"
-          content={`https://melvinliu.com${router.asPath}`}
-        />
-        <link rel="canonical" href={`https://melvinliu.com${router.asPath}`} />
-        <link rel="icon" href="/static/images/favicon.svg" />
-        <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content="Melvin Liu" />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@mlven23" />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
-        {meta.date && (
-          <meta property="article:published_time" content={meta.date} />
-        )}
-      </Head>
+      {!isBlogSlug && (
+        <Head>
+          <title>{meta.title}</title>
+          <meta name="robots" content="follow, index" />
+          <meta content={meta.description} name="description" />
+          <meta
+            property="og:url"
+            content={`https://melvinliu.com${router.asPath}`}
+          />
+          <link
+            rel="canonical"
+            href={`https://melvinliu.com${router.asPath}`}
+          />
+          <link rel="icon" href="/static/images/favicon.svg" />
+          <meta property="og:type" content={meta.type} />
+          <meta property="og:site_name" content="Melvin Liu" />
+          <meta property="og:description" content={meta.description} />
+          <meta property="og:title" content={meta.title} />
+          <meta property="og:image" content={meta.image} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@mlven23" />
+          <meta name="twitter:title" content={meta.title} />
+          <meta name="twitter:description" content={meta.description} />
+          <meta name="twitter:image" content={meta.image} />
+          {meta.date && (
+            <meta property="article:published_time" content={meta.date} />
+          )}
+        </Head>
+      )}
       <div
         className={cn(
           onTop
