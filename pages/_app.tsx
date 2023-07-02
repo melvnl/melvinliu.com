abfece2834321c650ca73fd6cfce6b6049988eb9
melvnl <melvinlim2000@gmail.com>
2023-07-02 12:38:13 +0700

    update: add page navigation progress bar for better user experience

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,13 @@ import { ThemeProvider } from "next-themes";
 
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import PageProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <ThemeProvider attribute="class">
+        <PageProgress color="#E16259" />
         <Component {...pageProps} />
       </ThemeProvider>
     </SessionProvider>
